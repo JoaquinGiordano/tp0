bc5555beb90b99b893f75bcae22f94dd86752dcf
Joaquin Giordano <joaquin_giordano@outlook.com>
2025-03-13 18:32:09 +0000

    Acomodando ubicación del config

--- client/src/client.c
+++ client/src/client.c
@@ -78,7 +78,7 @@ t_log* iniciar_logger(void)
 t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
-	nuevo_config = config_create("./cliente.config");
+	nuevo_config = config_create("../cliente.config");
 	return nuevo_config;
 }
 
